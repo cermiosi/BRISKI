fccb94346d26fc7c3df98616e660a7593ec65143
cermiosi <ferarifur@googlemail.com>
2024-12-05 11:38:25 +0100

    Revert "Adding makefile targets to generate mmi file, update instr-and-data mem in the bitstream and a few more related updates for ease of use"
    This reverts commit 5b311994d5f6f85553ee69bae1763d01ba0a2c49.

--- hardware/rtl/core_dummy_wrapper.sv
+++ hardware/rtl/core_dummy_wrapper.sv
@@ -1,8 +1,7 @@
 
 `include "riscv_pkg.sv"
 module core_dummy_wrapper #(
-    parameter int MMCM_OUT_FREQ = `MMCM_OUT_FREQ_MHZ,
-    parameter string HEX_PROG = "../../../software/runs/test_bitwise.inst"
+    parameter int MMCM_OUT_FREQ = `MMCM_OUT_FREQ_MHZ
 ) (
     output logic DONE_GPIO_LED_0,
     input  logic REFCLK_P,
